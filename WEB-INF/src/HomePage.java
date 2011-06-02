4e5e2786ae5b24fb8e89d378ec94ad2bc238f535
pjq <pengjianqing@gmail.com>
2011-06-02 17:37:23 +0800

    Add HomePage

---
+++ WEB-INF/src/HomePage.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.impjq.base.BaseHttpServlet;
+
+/**
+ * The HomePage.
+ * 
+ * @author pjq0274
+ */
+public class HomePage extends BaseHttpServlet {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4397272680358028385L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+        // TODO Auto-generated method stub
+        super.doGet(req, resp);
+
+        sendHomePage();
+    }
+
+    private void sendHomePage() {
+        // TODO Auto-generated method stub
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>FTClient Web HomePage</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h3>FTClient Web HomePage</h3>");
+        out.println("<a href=\"https://216.24.194.197:8443/HelloServlet/account/web/Register\">Register</a>");
+        out.println("<br>");
+        out.println("<a href=\"https://216.24.194.197:8443/HelloServlet/account/web/Update\">Update Info</a>");
+        out.println("<br>");
+        out.println("</body>");
+        out.println("</html>");
+
+    }
+}
