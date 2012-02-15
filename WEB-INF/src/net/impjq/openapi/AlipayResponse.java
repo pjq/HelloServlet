cfeefc18bbc18bf0644b423077e43023e4cb6501
pjq <pengjianqing@gmail.com>
2012-02-15 13:52:21 +0800

    add AlipayResponse

---
+++ WEB-INF/src/net/impjq/openapi/AlipayResponse.java
@@ -0,0 +1,96 @@
+package net.impjq.openapi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.URLDecoder;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.impjq.base.BaseHttpServlet;
+import net.impjq.util.Utils;
+
+public class AlipayResponse extends BaseHttpServlet {	
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+
+		resp.setContentType("text/html;charset=UTF-8");
+		// Init the common vars.
+		//out = resp.getWriter();
+		//mPrintWriter = out;
+
+		//String request = Utils.readFromInputStream(req.getInputStream());
+		//mRequestHashMap = parserPostParameters(request);
+
+		String domain = req.getParameter( "domain"); 
+
+		mPrintWriter.println("#Resolve domain:"+domain);
+		InetAddress[] addresses = InetAddress.getAllByName(domain);
+		int length = addresses.length;
+		for (int i = 0; i < length; i++) {
+			out.println(domain+"    "+addresses[i].getHostAddress());
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+
+	private String[] parser(String parameters) {
+		String params[] = parameters.split("&");
+
+		return params;
+	}
+
+	/**
+	 * Parser the post parameters.
+	 * 
+	 * @param parameters
+	 * @return
+	 */
+	protected HashMap<String, String> parserPostParameters(String parameters) {
+		String[] params = parser(parameters);
+
+		int length = params.length;
+
+		if (length <= 0) {
+			return null;
+		}
+
+		HashMap<String, String> hashMap = new HashMap<String, String>();
+		for (String p : params) {
+			String[] map = p.split("=");
+			if (map.length != 2) {
+				continue;
+			}
+			hashMap.put(map[0], urlDecode(map[1]));
+		}
+
+		return hashMap;
+	}
+
+	public static String urlDecode(String str) {
+		String result = str;
+		try {
+			result = URLDecoder.decode(str, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+}
